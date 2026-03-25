9a9cb897b1e344a9bb05896f41032b08be03f711
BestUSERNAME15361536 <97462120+BestUSERNAME15361536@users.noreply.github.com>
2026-03-25 17:09:33 -0500

    change app launcher

--- GIT/APPS/apps-launcher.sh
+++ GIT/APPS/apps-launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 printf "Minesweeper\nSpace Cadet Pinball\nMS Paint" | wofi --dmenu --prompt "Choose App" \
-| xargs -I{} bash -c 'case "$0" in "Minesweeper") wine ~/Desktop/APPS/winmine.exe ;; "Space Cadet Pinball") spacecadetpinball ;; "MS Paint") wine ~/Desktop/APPS/mspaint.exe ;; esac' {}
+| xargs -I{} bash -c 'case "$0" in "Minesweeper") wine GIT/APPS/winmine.exe ;; "Space Cadet Pinball") spacecadetpinball ;; "MS Paint") wine GIT/APPS/mspaint.exe ;; esac' {}
